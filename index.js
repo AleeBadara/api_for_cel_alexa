249bfbcaaa2c9b6aa8366a78e6d512aee63515be
Alioune Badara <mbaye.alioune.badara@gmail.com>
2019-02-10 19:17:58 +0100

    revue de code

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.patch('/majEnvBranche/:env', (req, res) => {
 
 });
 
-const PORT= process.env || 3003
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
-    console.log('app listening at port '+PORT);
+    console.log('app listening at port ' + PORT);
 })
